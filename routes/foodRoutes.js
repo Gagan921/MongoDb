bd8e2859d700e0caa0eb10e7b22eca094fe9d523
Gagan921 <63442672+Gagan921@users.noreply.github.com>
2022-01-06 05:29:58 +0530

    foodRoutes.js updated with POST

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const res = require('express/lib/response');
+
 const foodModel = require("../models/food");
 const app = express();
 
-app.get("/foods", async (request, response) => {
+app.get("/foods", async (req, res) => {
   const foods = await foodModel.find({});
 
   try {
-    response.send(foods);
+    res.send(foods);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+
+app.post("/food", async (request, response) => {
+  const food = new foodModel(request.body);
+
+  try {
+    await food.save();
+    response.send(food);
   } catch (error) {
     response.status(500).send(error);
   }
 });
+
 
 module.exports = app;
