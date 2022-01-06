c63b4a026a6a815610917d023f9f036e018bcc2c
Gagan921 <63442672+Gagan921@users.noreply.github.com>
2022-01-06 05:44:44 +0530

    server.js Updated

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const foodRouter = require("./routes/foodRoutes.js");
+const dotenv = require('dotenv');
+dotenv.config({path:'./config/.env'})
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://gagan921:12345@gettingstarted.om6j1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }
-);
-
+const connectDB= async()=>{
+  const conn = await mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true 
+  });
+console.log("It is connected")
+}
+connectDB();
 app.use(foodRouter);
 
 app.listen(3000, () => {
